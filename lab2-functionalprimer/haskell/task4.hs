c3e12d4f601faa9769016379ca121dbc3ba983ab
Amandeep Singh Dhaliwal <asdhaliwal4@hotmail.com>
2025-01-29 15:16:24 +0000

    task4

--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -1,14 +1,16 @@
 ask :: String -> IO ()
-ask prompt =
-  do
-  putStrLn prompt
-  line <- getLine
-  if line == ""
-    then ask prompt
-    else putStrLn ("you said: " ++ reverse line)
+ask prompt = do
+    putStrLn prompt
+    line <- getLine
+    if line == "quit"
+        then putStrLn "quitting..."
+        else if line == ""
+            then ask prompt
+            else do
+                putStrLn ("you said: " ++ reverse line)
+                ask prompt  -- Keep prompting until "quit" is entered
 
 main :: IO ()
-main =
-  do
-  let prompt = "please say something"
-  ask prompt
+main = do
+    let prompt = "please say something"
+    ask prompt
